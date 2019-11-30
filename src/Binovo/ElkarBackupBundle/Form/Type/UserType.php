80656cb023e4249c74956b38cc70b2a89321d44a
parreitu <parreitu@protonmail.com>
2019-11-30 23:56:19 +0100

    Solved user form issues

--- src/Binovo/ElkarBackupBundle/Form/Type/UserType.php
+++ src/Binovo/ElkarBackupBundle/Form/Type/UserType.php
@@ -10,21 +10,30 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $t = $options['translator'];
-        $builder->add('username'    , 'text'    , array('label' => $t->trans('Name'     , array(), 'BinovoElkarBackup'),
+        $builder->add('username'    ,  TextType::class    , array('label' => $t->trans('Name'     , array(), 'BinovoElkarBackup'),
                                                         'attr'  => array('class'    => 'form-control')))
-                ->add('email'       , 'email'   , array('label' => $t->trans('Email'    , array(), 'BinovoElkarBackup'),
+                ->add('email'       ,  TextType::class   , array('label' => $t->trans('Email'    , array(), 'BinovoElkarBackup'),
                                                         'attr'  => array('class'    => 'form-control')))
-                ->add('isActive'    , 'checkbox', array('label' => $t->trans('Is active', array(), 'BinovoElkarBackup'),
+                ->add('isActive'    , CheckboxType::class, array('label' => $t->trans('Is active', array(), 'BinovoElkarBackup'),
                                                         'required' => false))
-                ->add('roles'      , 'collection', array('type' => 'choice',
+                ->add('roles'      , CollectionType::class, array('entry_type' => ChoiceType::class,
                                              //'label' => 'Profile type',
                                              //'attr' => array('class' => 'form-control'),
-                                             'options' => array(
+                                             'entry_options' => array(
                                                 'expanded' => false,
                                                 'choices' => array(
                                                         'ROLE_ADMIN' => 'Admin',
@@ -32,7 +41,7 @@ class UserType extends AbstractType
                                                 ),
                                              ),
                                           ))
-                ->add('newPassword', 'repeated', array('type' => 'password',
+                ->add('newPassword', RepeatedType::class , array('type' => PasswordType::class,
 			                                                 'options' => array('attr' => array('class' => 'password-field form-control')),
 			                                                 'required' => false,
 			                                                 'first_options'  => array('label' => $t->trans('New password' , array(), 'BinovoElkarBackup')),
